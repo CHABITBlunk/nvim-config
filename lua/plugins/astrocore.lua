c6b8ef7cbb735a58e16af357484d68c4492329c7
chabitblunk <cblunk097@gmail.com>
2024-10-14 17:12:40 -0600

    get rid of insert-mode jk and jj keybindings

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -75,14 +75,6 @@ return {
         -- quick save
         -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
       },
-      i = {
-        ["jk"] = {
-          "<esc>",
-        },
-        ["kj"] = {
-          "<esc>",
-        },
-      },
       t = {
         -- setting a mapping to false will disable it
         -- ["<esc>"] = false,
