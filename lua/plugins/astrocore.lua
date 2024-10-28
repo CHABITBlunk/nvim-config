71b7fe886247af7b57c2cf1a40b627cf5bec3cd3
chabitblunk <cblunk097@gmail.com>
2024-10-28 17:37:11 -0600

    set C-c to escape; update lazy-lock

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -69,6 +69,7 @@ return {
           end,
           desc = "Pick to close",
         },
+        ["<C-c>"] = { "<esc>", desc = "Return to normal mode" },
         -- tables with just a `desc` key will be registered with which-key if it's installed
         -- this is useful for naming menus
         ["<Leader>b"] = { desc = "Buffers" },
